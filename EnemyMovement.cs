31059c2ec6b8257a2fb2179594f3bc5b95b11b44
beingAaksh <74947468+beingAaksh@users.noreply.github.com>
2020-12-07 10:52:00 +0530

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private AggroDetection aggroDetection;
+    private Transform target;
+
+    private void Awake()
+    {
+
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        aggroDetection = GetComponent<AggroDetection>();
+        aggroDetection.OnAggro += AggroDetection_OnAggro;
+
+    }
+
+    private void AggroDetection_OnAggro(Transform target)
+    {
+        this.target = target;
+      
+    }
+
+    private void Update()
+    {
+        if ( target != null)
+        {
+            navMeshAgent.SetDestination(target.position);
+            float currentSpeed = navMeshAgent.velocity.magnitude;
+            animator.SetFloat("Speed", currentSpeed);
+        }
+       
+    }
+}
+
---
+++ PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    [SerializeField]
+    private float forwardMoveSpeed = 7.5f;
+    [SerializeField]
+    private float backwardMoveSpeed = 3;
+    [SerializeField]
+    private float turnSpeed = 150f;
+    private CharacterController characterController;
+    private Animator animator;
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+       
+    }
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+     var vertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(horizontal, 0, vertical);
+      
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        if( vertical != 0)
+        {
+            float moveSpeedToUse = (vertical > 0) ? forwardMoveSpeed : backwardMoveSpeed;
+            characterController.SimpleMove(transform.forward * moveSpeedToUse * vertical);
+        }
+        
+    }
+}
